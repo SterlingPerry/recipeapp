11d9071e179827fa68d1057af745f155b7c012ce
SterlingPerry <sterling.perry@georgiacourts.gov>
2019-01-08 16:27:31 -0500

    Updates

---
+++ precache-manifest.1467721c481ced6344657390e0e0ca72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7701815b74baf89f84",
+    "url": "/recipeapp/static/js/runtime~main.af770181.js"
+  },
+  {
+    "revision": "3f8a2c7ef4015d3e25d7",
+    "url": "/recipeapp/static/js/main.3f8a2c7e.chunk.js"
+  },
+  {
+    "revision": "3912e494b4cc3a5d3eda",
+    "url": "/recipeapp/static/js/1.3912e494.chunk.js"
+  },
+  {
+    "revision": "3f8a2c7ef4015d3e25d7",
+    "url": "/recipeapp/static/css/main.40f79b14.chunk.css"
+  },
+  {
+    "revision": "fc32ec20c21822d5a2ce3fd6db57609a",
+    "url": "/recipeapp/index.html"
+  }
+];
